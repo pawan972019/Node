4a275f86d98b983bcfb16ac61fb7dd04b475ad2c
pawan.jha <pawan.jha@jetsynthesys.com>
2022-01-28 16:51:14 +0530

    added atlas mongo uri

--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/mongoTest')
-    .then(() => console.log("Mongo Connection created"))
+var db = "mongodb+srv://mongo-pawan:sRjWq5ce5rsdzrn@mongodb-tutorial.uch7h.mongodb.net/expense?retryWrites=true&w=majority"
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Mongo Connection created"))
     .catch((err) => console.log(err))
+
