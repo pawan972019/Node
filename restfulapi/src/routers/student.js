7a72d25d56018469e9ae0a82fbde9c4ec66da16d
pawan.jha <pawan.jha@jetsynthesys.com>
2022-02-03 19:22:31 +0530

    json response correct in api response

--- restfulapi/src/routers/student.js
+++ restfulapi/src/routers/student.js
@@ -9,6 +9,17 @@ router.get('/node', (req, res) => {
     res.send('Hello whatsup guys')
 })
 
+const errorResponse = () => {
+
+    let errorRes = {
+        "code": 400,
+        "status": "fail",
+        "message": "Data not found"
+    }
+
+    return errorRes
+}
+
 router.post('/students', async (req, res) => {
 
     try {
@@ -29,10 +40,19 @@ router.post('/students', async (req, res) => {
 
         //this code will be there if we want use async and await
         var createduser = await student.save()
-        res.status(201).send(createduser)
+
+        let response = {
+            "code": 200,
+            "status": "success",
+            "message": "Student saved successfully",
+        }
+
+        console.log("output: " + response)
+        res.status(201).send(response)
 
     } catch (e) {
-        res.status(400).send(e)
+
+        res.status(400).send(errorResponse())
         console.log(e);
     }
 })
@@ -42,9 +62,25 @@ router.get('/students', async (req, res) => {
 
     try {
         var studentdata = await Student.find()
-        res.status(200).send(studentdata)
+
+        if (studentdata) {
+            let response = {
+                "code": 200,
+                "status": "success",
+                "message": "Data send successfully",
+                "data": {
+                    studentList: studentdata
+                }
+            }
+            console.log(response)
+            res.status(200).send(response)
+        } else {
+            res.status(404).send(errorResponse())
+        }
+
     } catch (e) {
-        res.status(400).send(e)
+
+        res.status(400).send(errorResponse())
         console.log(e);
     }
 })
@@ -57,12 +93,22 @@ router.get('/students/:id', async (req, res) => {
 
         var studentdata = await Student.findById({ _id })
         if (studentdata) {
-            res.status(200).send(studentdata)
+
+            let response = {
+                "code": 200,
+                "status": "success",
+                "message": "Data send successfully",
+                "data": {
+                    student: studentdata
+                }
+            }
+            res.status(200).send(response)
         } else {
-            res.status(404).send()
+            res.status(404).send(errorResponse())
         }
     } catch (e) {
-        res.status(400).send(e)
+
+        res.status(400).send(errorResponse())
         console.log(e);
     }
 })
@@ -77,13 +123,23 @@ router.patch('/students/:id', async (req, res) => {
             new: true
         })
         if (studentdata) {
-            res.status(200).send(studentdata)
+            let response = {
+                "code": 200,
+                "status": "success",
+                "message": "Data update successfully",
+                "data": {
+                    student: studentdata
+                }
+            }
+            console.log(studentdata)
+            res.status(200).send(response)
         } else {
-            res.status(404).send()
+            console.log(`Error while update in elss: ${studentdata}`)
+            res.status(404).send(errorResponse())
         }
     } catch (e) {
-        res.status(400).send(e)
-        console.log(e);
+        console.log(`Error while update in catch block: ${e}`)
+        res.status(400).send(errorResponse())
     }
 
 })
@@ -96,13 +152,22 @@ router.delete('/students/:id', async (req, res) => {
         var studentdata = await Student.findByIdAndDelete(_id)
         console.log(studentdata);
         if (studentdata) {
-            res.status(200).send(studentdata)
+            let response = {
+                "code": 200,
+                "status": "success",
+                "message": "Data deleted successfully",
+                "data": {
+                    student: studentdata
+                }
+            }
+            console.log(studentdata)
+            res.status(200).send(response)
         } else {
-            res.status(400).statusMessage("Data not found").send()
+            res.status(400).send(errorResponse())
         }
 
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send(errorResponse())
         console.log(e);
     }
 
